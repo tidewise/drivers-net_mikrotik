bc4a5e4bf4a8293c530ed9c6494fdcbaeac898a1
Jhonas <jhonas.moura@tidewise.io>
2022-12-14 16:45:06 -0300

    fix: clang-format library template

--- src/Dummy.hpp
+++ src/Dummy.hpp
@@ -1,16 +1,14 @@
 #ifndef _DUMMYPROJECT_DUMMY_HPP_
 #define _DUMMYPROJECT_DUMMY_HPP_
 
-namespace net_mikrotik
-{
-    class DummyClass
-    {
-        public:
-            /**
-             * Print a welcome to stdout
-             * \return nothing
-             */
-            void welcome();
+namespace net_mikrotik {
+    class DummyClass {
+    public:
+        /**
+         * Print a welcome to stdout
+         * \return nothing
+         */
+        void welcome();
     };
 
 } // end namespace net_mikrotik
